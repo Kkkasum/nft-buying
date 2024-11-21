4d20e2a0d9abbd3082152afd0842e2a60f2d672e
Kasum <kasum2605@mail.ru>
2024-11-21 17:13:51 +0400

    init commit

---
+++ scripts/mintNftItem.ts
@@ -0,0 +1,39 @@
+import { compile, NetworkProvider } from '@ton/blueprint';
+import { Address, beginCell, toNano } from '@ton/core';
+import { contentToCell, NftCollection, royaltyParamsToCell } from '../wrappers/NftCollection';
+
+export async function run(provider: NetworkProvider) {
+    const ownerAddress = Address.parse('EQB_MpZaOhVMdN4Q6NsRCGYpHsOYqxiEuqIGsyUhweQnaehv');
+    const royaltyAddress = Address.parse('EQB_MpZaOhVMdN4Q6NsRCGYpHsOYqxiEuqIGsyUhweQnaehv');
+    const feeAddress = Address.parse('EQB_MpZaOhVMdN4Q6NsRCGYpHsOYqxiEuqIGsyUhweQnaehv');
+
+    const contentCell = contentToCell(
+        'https://starsfinance.fra1.digitaloceanspaces.com/nft/collection.json',
+        'https://starsfinance.fra1.digitaloceanspaces.com/nft/items/',
+    );
+    const royaltyParamsCell = royaltyParamsToCell(10, 100, royaltyAddress);
+
+    const nftMinter = provider.open(
+        NftCollection.createFromConfig(
+            {
+                ownerAddress: ownerAddress,
+                nextItemIndex: 0,
+                content: contentCell,
+                nftItemCode: await compile('NftItem'),
+                royaltyParams: royaltyParamsCell,
+                purchaseFee: toNano('1'),
+                feeAddress: feeAddress,
+            },
+            await compile('NftCollection'),
+        ),
+    );
+
+    const { nextItemIndex } = await nftMinter.getCollectionData();
+
+    await nftMinter.sendMint(provider.sender(), {
+        value: toNano('0.1'),
+        itemIndex: nextItemIndex,
+        amount: toNano('0.05'),
+        nftContent: beginCell().storeBuffer(Buffer.from('common.json')).endCell(),
+    });
+}
